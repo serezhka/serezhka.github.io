1b1459ec2ec10fd380c989576506807d95d6bc41
Sergio Fedorini <srzhkax@gmail.com>
2024-04-26 13:00:20 +0300

    Update clicker.js

--- hamsterkombat/js/clicker.js
+++ hamsterkombat/js/clicker.js
@@ -2,17 +2,11 @@
 // https://hamsterkombat.io/js/telegram-web-app.js -> https://serezhka.github.io/hamsterkombat/js/telegram-web-app.js
 
 (function () {
+    const evt1 = new PointerEvent('pointerdown', {clientX: 150, clientY: 300});
+    const evt2 = new PointerEvent('pointerup', {clientX: 150, clientY: 300});
     setInterval((function fn() {
-        const evt1 = new PointerEvent('pointerdown', {
-            clientX: 100,
-            clientY: 400,
-        });
-        const evt2 = new PointerEvent('pointerup', {
-            clientX: 100,
-            clientY: 400,
-        });
         const energy = parseInt(document.getElementsByClassName("user-tap-energy")[0].getElementsByTagName("p")[0].textContent.split(" / ")[0]);
-        if (energy > 50) {
+        if (energy > 25) {
             document.getElementsByClassName('user-tap-button')[0].dispatchEvent(evt1);
             document.getElementsByClassName('user-tap-button')[0].dispatchEvent(evt2);
         }
